373a0ec9b9d732b09a0602a39caa177d31d3d0fb
Eli Bendersky <eliben@chromium.org>
2013-10-25 15:29:25 -0700

    Refactor the emission a bit and add way to allocate RW memory, and then set RX permissions on it for execution.

--- basic_self_jit_x64.c
+++ basic_self_jit_x64.c
@@ -8,9 +8,12 @@
 //------------------------------------------------------------------------------
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/mman.h>
 
 
+// Allocates RWX memory of given size and returns a pointer to it. On failure,
+// prints out the error and returns NULL.
 void* alloc_executable_memory(size_t size) {
   void* ptr = mmap(0, size,
                    PROT_READ | PROT_WRITE | PROT_EXEC,
@@ -22,21 +25,47 @@ void* alloc_executable_memory(size_t size) {
   return ptr;
 }
 
+// Allocates RW memory of given size and returns a pointer to it. On failure,
+// prints out the error and returns NULL. Unlike malloc, the memory is allocated
+// on a page boundary so it's suitable for calling mprotect.
+void* alloc_writable_memory(size_t size) {
+  void* ptr = mmap(0, size,
+                   PROT_READ | PROT_WRITE,
+                   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  if (ptr == (void*)-1) {
+    perror("mmap");
+    return NULL;
+  }
+  return ptr;
+}
+
+// Sets a RX permission on the given memory, which must be page-aligned. Returns
+// 0 on success. On failure, prints out the error and returns -1.
+int make_memory_executable(void* m, size_t size) {
+  if (mprotect(m, size, PROT_READ | PROT_EXEC) == -1) {
+    perror("mprotect");
+    return -1;
+  }
+  return 0;
+}
+
 void emit_code_into_memory(unsigned char* m) {
-  // mov %rdi, %rax
-  *m++ = 0x48; *m++ = 0x89; *m++ = 0xf8;
-  // add $4, %rax
-  *m++ = 0x48; *m++ = 0x83; *m++ = 0xc0; *m++ = 0x28;
-  // ret
-  *m++ = 0xc3;
+  unsigned char code[] = {
+    0x48, 0x89, 0xf8,                   // mov %rdi, %rax
+    0x48, 0x83, 0xc0, 0x28,             // add $4, %rax
+    0xc3                                // ret
+  };
+  memcpy(m, code, sizeof(code));
 }
 
 typedef long (*JittedFunc)(long);
 
 
 int main(int argc, char** argv) {
-  void* m = alloc_executable_memory(1024);
+  const size_t SIZE = 1024;
+  void* m = alloc_writable_memory(SIZE);
   emit_code_into_memory(m);
+  make_memory_executable(m, SIZE);
 
   // Finally, run the "JITed" code as a long (*)(long), passing it an argument
   JittedFunc func = m;
